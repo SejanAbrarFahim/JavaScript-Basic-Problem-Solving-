5ebce2ef3f04ae9c3896ca95773aa48fd299a785
Sejan Abrar Fahim <sejan62007@gmail.com>
2026-03-04 15:33:44 +0600

    update

--- Problem Solving 1.js
+++ Problem Solving 1.js
@@ -34,3 +34,13 @@
   }
 
   myFunction();
+
+
+
+// 06. Create an object called person with name = John, age = 50, Then, access the object to alert("John is 50").
+  const person = {
+    name : "John",
+    age : 50 
+  };
+
+  alert(person.name + " is " + person.age);
